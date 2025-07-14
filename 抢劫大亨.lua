501b10df72bae0ee65e886dce18d987d12a1cec6
ZIONPCE <3891722440@qq.com>
2025-07-15 01:21:36 +0800

    Add files via upload

---
+++ 抢劫大亨.lua
@@ -0,0 +1,9 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+-- Subscribe to TaRz Game TV on YouTube
+-- Credits to the Owner
+
+while wait() do
+game:GetService("ReplicatedStorage").Knit.Services.TycoonService.RF.PayIncome:InvokeServer(game:GetService("Players").LocalPlayer)
+end

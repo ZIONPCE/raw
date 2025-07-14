26fc5570ab29cb50fd4b2042eb3ee1abc265c43b
ZIONPCE <3891722440@qq.com>
2025-07-15 01:27:29 +0800

    Add files via upload

---
+++ 军事角色扮演.lua
@@ -0,0 +1,10 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+-- Execute one at a time -- 
+
+game:GetService("ReplicatedStorage").Configuration["Core Systems"].Datastore.GiveCurrency:FireServer({ Type = "Diamond", Amount = 9e9 })
+
+-- 
+
+game:GetService("ReplicatedStorage").Configuration["Core Systems"].Datastore.GiveCurrency:FireServer({ Type = "Money", Amount = 9e9 })
